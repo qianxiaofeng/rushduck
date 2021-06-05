dec24354ee5e22a8c1bae2ad8ca1af0659c54adf
deadlock <mhfred.deadlock@gmail.com>
2021-06-05 15:52:41 +0800

    init rush duck

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>冲鸭</title>
+        <meta name="description" content="冲鸭" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>hello world</h1>
+      </main>
+
+    </div>
+  )
+}
