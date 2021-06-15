57bc4db24c5f931cf9a06059cb54019c62665911
deadlock <mhfred.deadlock@gmail.com>
2021-06-15 21:56:06 +0800

    fix build error

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -7,7 +7,6 @@ import Link from 'next/link'
 const CustomLink = (props) => {
     const href = props.href;
     const isInternalLink = href && (href.startsWith('/') || href.startsWith('#'));
-    console.log(href);
     if (isInternalLink) {
         return (
             <Link href={href}>
--- next.config.js
+++ next.config.js
@@ -3,5 +3,14 @@ const withMDX = require("@next/mdx")({
 })
 
 module.exports = withMDX({
+    webpack: (config, {isServer}) => {
+        if (!isServer) {
+            config.resolve.fallback.fs = false;
+        }
+        return config;
+    },
+    future: {
+        webpack5: true,
+    },
     pageExtensions: ['js', 'jsx', 'md', 'mdx']
 })
